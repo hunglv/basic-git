392e0dd6b80920c396e5ef94065b22f19fd0e1c1
hunglv <hunglv@qsoftvietnam.com>
2013-12-19 17:16:11 +0700

    Add file on hunglv branch

---
+++ git-demo/git-demo/OtherClass.h
@@ -0,0 +1,13 @@
+//
+//  OtherClass.h
+//  git-demo
+//
+//  Created by Cừu Lười on 12/19/13.
+//  Copyright (c) 2013 Cừu Lười. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OtherClass : NSObject
+
+@end
---
+++ git-demo/git-demo/OtherClass.m
@@ -0,0 +1,13 @@
+//
+//  OtherClass.m
+//  git-demo
+//
+//  Created by Cừu Lười on 12/19/13.
+//  Copyright (c) 2013 Cừu Lười. All rights reserved.
+//
+
+#import "OtherClass.h"
+
+@implementation OtherClass
+
+@end
