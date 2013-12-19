7c5d5560e40fc4863862c958eabdf2707368f290
hunglv <hunglv@qsoftvietnam.com>
2013-12-19 18:47:08 +0700

    Add file on master

---
+++ git-demo/git-demo/ObjectFirst.h
@@ -0,0 +1,13 @@
+//
+//  ObjectFirst.h
+//  git-demo
+//
+//  Created by Cừu Lười on 12/19/13.
+//  Copyright (c) 2013 Cừu Lười. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectFirst : NSObject
+
+@end
---
+++ git-demo/git-demo/ObjectFirst.m
@@ -0,0 +1,13 @@
+//
+//  ObjectFirst.m
+//  git-demo
+//
+//  Created by Cừu Lười on 12/19/13.
+//  Copyright (c) 2013 Cừu Lười. All rights reserved.
+//
+
+#import "ObjectFirst.h"
+
+@implementation ObjectFirst
+
+@end
