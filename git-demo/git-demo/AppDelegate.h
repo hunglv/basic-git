5c0b68d1ee4bb65d4ddc7e5448ce60726b3ca26a
hunglv <hunglv@qsoftvietnam.com>
2013-12-19 15:08:11 +0700

    init project

---
+++ git-demo/git-demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git-demo
+//
+//  Created by Cừu Lười on 12/19/13.
+//  Copyright (c) 2013 Cừu Lười. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
